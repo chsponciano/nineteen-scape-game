0bbf583fa976df97c12fa3927adaed61a012d38f
chsponciano <chs.ponciano@gmail.com>
2020-10-05 22:37:25 -0300

    initial commit of the game project

---
+++ CODIGO-FONTE/nineteen-scape-game/Assets/Scripts/Player.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public const float playerLimit = 3f;
+    public const float playerFrameMovement = 0.1f;
+
+    public float speed;
+    public float horizontalSpeed;
+    public float jumpHeight;
+    public float gravity;
+    public float rayRadius;
+    public LayerMask layer;
+    public Animator anime;
+
+    private CharacterController controller;
+    private float jumpVelocity;
+    private bool isMovingLeft;
+    private bool isMovingRight;
+    private bool isDead = false;
+    private GameController gameController;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = Vector3.forward * speed;   
+        
+        if(controller.isGrounded) 
+        {
+            if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                jumpVelocity = jumpHeight;
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < playerLimit && !isMovingRight)
+            {
+                isMovingRight = true;
+                StartCoroutine(RightMove());
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > -playerLimit && !isMovingLeft)
+            {
+                isMovingLeft = true;
+                StartCoroutine(LeftMove());
+            }
+        }
+        else
+        {
+            jumpVelocity -= gravity;
+        }
+
+        OnCollision();
+        direction.y = jumpVelocity;
+        controller.Move(direction * Time.deltaTime);
+    }
+
+    IEnumerator LeftMove()
+    {
+        for(float i = 0; i < 10; i += playerFrameMovement)
+        {
+            controller.Move(Vector3.left * Time.deltaTime * horizontalSpeed);
+            yield return null;
+        }
+
+        isMovingLeft = false;
+    }
+
+    IEnumerator RightMove()
+    {
+        for(float i = 0; i < 10; i += playerFrameMovement)
+        {
+            controller.Move(Vector3.right * Time.deltaTime * horizontalSpeed);
+            yield return null;
+        }
+
+        isMovingRight = false;
+    }
+
+    void OnCollision()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayRadius, layer) && !isDead)
+        {
+            anime.SetTrigger("die");
+            speed = 0;
+            jumpHeight = 0;
+            horizontalSpeed = 0;
+            Invoke("AnimeGameOver", 3f);
+            isDead = true;
+        }
+    }
+
+    void AnimeGameOver()
+    {
+        gameController.CallGameOver();
+    }
+}
---
+++ CODIGO-FONTE/nineteen-scape-game/Assets/Scripts/SpawnPlatform.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : MonoBehaviour
+{
+    public const int platformSize = 86;
+
+    public List<GameObject> platforms = new List<GameObject>();
+    public List<Transform> currentPlatforms = new List<Transform>();
+
+    public int offset;
+    private Transform player;
+    private Transform currentPlatformPoint;
+    private int platformIndex;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for(int i = 0; i < platforms.Count; i++)
+        {
+            Transform platform = Instantiate(platforms[i], new Vector3(0, 0, i * platformSize), transform.rotation).transform;
+            currentPlatforms.Add(platform);
+            offset += 86;
+        }
+
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
+    }
+
+    void Update()
+    {
+        float distance = player.position.z - currentPlatformPoint.position.z;
+        if (distance >= 5)
+        {
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if(platformIndex > currentPlatforms.Count -1) 
+            {
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().point;
+        }
+    }
+
+    public void Recycle(GameObject platform)
+    {
+        platform.transform.position = new Vector3(0, 0, offset);
+        offset += platformSize;
+    }
+}
