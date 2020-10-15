e3e2339f33b6615f8be898881034587776f11620
Vinícius Luis da Silva <vinicius.lds.br@gmail.com>
2020-10-15 20:26:09 -0300

    Adicionado menu

---
+++ 2020-10-15 - aula09 Avaliacao1/CODIGO-FONTE/nineteen-scape-game/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuControl : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private GameObject CanvasCreditsMenu;
+    private GameObject CanvasStartMenu;
+
+    public void CreditsButton()
+    {
+        CanvasCreditsMenu.SetActive(true);
+        CanvasStartMenu.SetActive(false);
+    }
+
+    public void BackStartMenuStart()
+    {
+        CanvasCreditsMenu.SetActive(false);
+        CanvasStartMenu.SetActive(true);
+    }
+
+    void Start()
+    {
+        CanvasCreditsMenu = GameObject.Find("CanvasCreditsMenu").gameObject;
+        CanvasStartMenu = GameObject.Find("CanvasStartMenu").gameObject;
+
+        this.BackStartMenuStart();
+    }
+
+    public void QuitGameButton()
+    {
+        Application.Quit();
+    }
+
+    private void Load()
+    {
+            
+    }
+
+}
