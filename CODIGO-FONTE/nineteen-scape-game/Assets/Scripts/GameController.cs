5ac9f88b3a0f6b44a8cd147bffc719d979114167
chsponciano <chs.ponciano@gmail.com>
2020-10-21 20:29:12 -0300

    reset repositore

---
+++ CODIGO-FONTE/nineteen-scape-game/Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public float score;
+    public float speedIncreaseRate;
+    public float horizontalSpeedIncreaseRate;
+
+    public List<GameObject> adversaries = new List<GameObject>();
+    public GameObject gameOver;
+    public Text scoreText;
+    public Text gameOverScoreText;
+
+    private Player player;
+    private float t;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        if(!player.isDead)
+        {
+            score += Time.deltaTime * 5f;
+            scoreText.text = Mathf.Round(score).ToString() + "m";
+            player.speed += speedIncreaseRate;
+            player.horizontalSpeed += horizontalSpeedIncreaseRate;
+            randomOpponents();
+        }
+    }
+
+    public void CallGameOver()
+    {
+        gameOverScoreText.text = string.Format("Atingido {0}m", Mathf.Round(score));
+        gameOver.SetActive(true);
+    }
+
+    private void randomOpponents()
+    {
+        if(t > (50f - (player.speed / 100)))
+        {
+            Instantiate(adversaries[0], getRandomVector3(Random.Range(1, 4)), transform.rotation);
+            t = 0f;
+        }
+        else
+        {
+            t += 1f;
+        }
+    }
+
+    private Vector3 getRandomVector3(int position)
+    {
+        float x = -5;
+        float z = player.transform.position.z + 120f;
+
+        if (position == 2) 
+        {
+            x = 0;
+        }
+        else if (position == 3) 
+        {
+            x = 5;
+        }
+
+        return new Vector3(x, 6, z);
+    }
+}
---
+++ CODIGO-FONTE/nineteen-scape-game/Assets/Scripts/Player.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public const float playerLimit = 3f;
+    public const float playerFrameMovement = 0.1f;
+
+    public float speed;
+    public float horizontalSpeed;
+    public float jumpHeight;
+    public float gravity;
+    public float rayRadius;
+    public LayerMask layer;
+    public Animator anime;
+    public bool isDead = false;
+
+    private CharacterController controller;
+    private float jumpVelocity;
+    private bool isMovingLeft;
+    private bool isMovingRight;
+    private GameController gameController;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = Vector3.forward * speed;   
+        
+        if(controller.isGrounded) 
+        {
+            if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                jumpVelocity = jumpHeight;
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < playerLimit && !isMovingRight)
+            {
+                isMovingRight = true;
+                StartCoroutine(RightMove());
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > -playerLimit && !isMovingLeft)
+            {
+                isMovingLeft = true;
+                StartCoroutine(LeftMove());
+            }
+        }
+        else
+        {
+            jumpVelocity -= gravity;
+        }
+
+        OnCollision();
+        direction.y = jumpVelocity;
+        controller.Move(direction * Time.deltaTime);
+    }
+
+    IEnumerator LeftMove()
+    {
+        float x = transform.position.x;
+
+        if (x > -5) 
+        {
+            while (transform.position.x >= x - 5f) 
+            {
+                controller.Move(Vector3.left * Time.deltaTime * horizontalSpeed);
+                yield return null;
+            }
+        }        
+        normalize();
+        isMovingLeft = false;        
+    }
+
+    IEnumerator RightMove()
+    {
+        float x = transform.position.x;
+
+        if (x < 5) 
+        {
+            while (transform.position.x <= x + 5f) 
+            {
+                controller.Move(Vector3.right * Time.deltaTime * horizontalSpeed);
+                yield return null;
+            }
+        }     
+        normalize();
+        isMovingRight = false;
+    }
+
+    private void normalize()
+    {
+        float x = transform.position.x;
+        float currentX;
+
+        if (x < -3f) 
+        {
+            currentX = -5f;
+        } 
+        else if (x > 3f) 
+        {
+            currentX = 5f;
+        }
+        else 
+        {
+            currentX = 0f;
+        }
+
+        transform.position = new Vector3(currentX, transform.position.y, transform.position.z);
+    }
+
+    void OnCollision()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayRadius, layer) && !isDead)
+        {
+            anime.SetTrigger("die");
+            speed = 0;
+            jumpHeight = 0;
+            horizontalSpeed = 0;
+            Invoke("AnimeGameOver", 3f);
+            isDead = true;
+        }
+    }
+
+    void AnimeGameOver()
+    {
+        gameController.CallGameOver();
+        // SceneManager.LoadScene(0);
+    }
+}
