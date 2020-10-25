9b7ea5f638cd00f0a49ec282209918ffbe17a916
chsponciano <chs.ponciano@gmail.com>
2020-10-25 15:53:20 -0300

    change character colors and layouts

--- CODIGO-FONTE/nineteen-scape-game/Assets/Scripts/Adversaries.cs
+++ CODIGO-FONTE/nineteen-scape-game/Assets/Scripts/Adversaries.cs
@@ -8,6 +8,7 @@ public class Adversaries : MonoBehaviour
     void Start()
     {
         transform.Rotate(0f, 180f, 0f);
+        // transform.localScale = new Vector3(0.8f, 0.7f, 1);
     }
 
     // Update is called once per frame
